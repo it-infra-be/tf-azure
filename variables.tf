5187fe741fb87541056dc74552062bea30194195
Karon Pante <karon.pante@it-infra.be>
2026-02-26 21:28:30 +0100

    refactoring

--- variables.tf
+++ variables.tf
@@ -200,7 +200,7 @@ variable "vpns" {
       instances = map(object({
         public_ip_address_id   = optional(string)
         bgp_apipa_addresses    = optional(list(string))
-      })))
+      }))
     })
 
     local_network_gateways = optional(map(object({
